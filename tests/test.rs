1b36aeee4af0e91f309942985325e512a697a1a0
Naoto Ikeno <ikenox@gmail.com>
2022-06-06 21:50:29 +0900

    update macro test case

--- tests/test.rs
+++ tests/test.rs
@@ -1,92 +1,80 @@
 #![feature(generic_associated_types)]
 
 #[test]
-fn derive_from_html() {
+fn derive() {
     use h2s::FromHtml;
 
     #[derive(FromHtml, Debug, Eq, PartialEq)]
-    pub struct Page {
+    pub struct Struct1 {
+        // attr
         #[h2s(attr = "lang")]
-        lang: String,
-        #[h2s(select = "h1.blog-title")]
-        blog_title: String,
-        #[h2s(select = ".articles > div", attr = "data-author")]
-        article_authors: Vec<String>,
-        #[h2s(select = ".articles > div > h2")]
-        article_titles: Vec<String>,
-        #[h2s(select = ".articles > div")]
-        articles: Vec<ArticleElem>,
-        #[h2s(select = "footer")]
-        footer: Footer,
+        a: String,
+        // select
+        #[h2s(select = ".b")]
+        b: String,
+        // attr & select
+        #[h2s(select = ".c", attr = "id")]
+        c: String,
+
+        // Vec
+        #[h2s(select = ".d > div")]
+        d: Vec<String>,
+        // Option
+        #[h2s(select = ".e")]
+        e: Option<String>,
+        // Array
+        #[h2s(select = ".f > div")]
+        f: [String; 3],
+        // Another derived struct
+        #[h2s(select = ".g")]
+        g: Struct2,
     }
 
     #[derive(FromHtml, Debug, Eq, PartialEq)]
-    pub struct ArticleElem {
-        #[h2s(select = "h2")]
-        title: String,
-        #[h2s(select = "p.modified-date")]
-        modified_date: Option<String>,
-    }
-
-    #[derive(FromHtml, Debug, Eq, PartialEq)]
-    pub struct Footer {
-        #[h2s]
-        txt: String,
+    pub struct Struct2 {
+        #[h2s()]
+        h: String,
     }
 
     let html = r#"
 <!DOCTYPE html>
-<html lang="en">
+<html lang="avalue">
 <body>
-<h1 class="blog-title">My tech blog</h1>
-<div class="articles">
-    <div data-author="Alice">
-        <h2>article1</h2>
-    </div>
-    <div data-author="Bob">
-        <h2>article2</h2>
-        <p class="modified-date">2020-05-01</p>
-    </div>
-    <div data-author="Ikeno">
-        <h2>article3</h2>
-    </div>
+<div class="b">bvalue</div>
+<div class="c" id="cvalue" />
+<div class="d">
+    <div>dvalue1</div>
+    <div>dvalue2</div>
+    <div>dvalue3</div>
 </div>
-<footer>this is footer</footer>
+<div class="e">evalue</div>
+<div class="f">
+    <div>fvalue1</div>
+    <div>fvalue2</div>
+    <div>fvalue3</div>
+</div>
+<div class="g">hvalue</div>
 </body>
 </html>
     "#;
 
-    let res = h2s::utils::parse::<Page>(html);
+    let res = h2s::utils::parse::<Struct1>(html);
     assert_eq!(
         res,
-        Ok(Page {
-            lang: "en".to_string(),
-            blog_title: "My tech blog".to_string(),
-            article_authors: vec!["Alice", "Bob", "Ikeno"]
-                .iter()
-                .map(|a| a.to_string())
-                .collect(),
-            article_titles: vec!["article1", "article2", "article3"]
-                .iter()
-                .map(|a| a.to_string())
-                .collect(),
-            articles: vec![
-                ArticleElem {
-                    title: "article1".to_string(),
-                    modified_date: None,
-                },
-                ArticleElem {
-                    title: "article2".to_string(),
-                    modified_date: Some("2020-05-01".to_string()),
-                },
-                ArticleElem {
-                    title: "article3".to_string(),
-                    modified_date: None,
-                }
-            ],
-            footer: Footer {
-                txt: "this is footer".to_string()
+        Ok(Struct1 {
+            a: "avalue".to_string(),
+            b: "bvalue".to_string(),
+            c: "cvalue".to_string(),
+            d: vec(["dvalue1", "dvalue2", "dvalue3"]),
+            e: Some("evalue".to_string()),
+            f: ["fvalue1", "fvalue2", "fvalue3"].map(|s| s.to_string()),
+            g: Struct2 {
+                h: "hvalue".to_string()
             }
         })
     )
+}
+
+fn vec<const N: usize>(arr: [&str; N]) -> Vec<String> {
+    arr.map(|s| s.to_string()).to_vec()
 }
