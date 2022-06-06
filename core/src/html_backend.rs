0d61fdebabd5f687c27e941e56b248c41d3ca021
Naoto Ikeno <ikenox@gmail.com>
2022-06-06 22:24:35 +0900

    add test case for scraper background

--- core/src/html_backend.rs
+++ core/src/html_backend.rs
@@ -1,5 +1,6 @@
 mod scraper {
     use crate::{HtmlElementRef, Selector};
+    use itertools::Itertools;
 
     impl Selector for scraper::Selector {
         fn parse<S: AsRef<str>>(s: S) -> Result<Self, String> {
@@ -16,11 +17,91 @@ mod scraper {
         }
 
         fn text_contents(&self) -> String {
-            self.text().collect::<String>()
+            self.text().join(" ")
         }
 
         fn get_attribute<S: AsRef<str>>(&self, attr: S) -> Option<&str> {
             self.value().attr(attr.as_ref())
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use crate::{HtmlElementRef, Selector};
+
+        #[test]
+        fn selector() {
+            assert_eq!(
+                <scraper::Selector as super::Selector>::parse("div > .a").unwrap(),
+                scraper::Selector::parse("div > .a").unwrap(),
+            );
+
+            assert_eq!(
+                <scraper::Selector as super::Selector>::parse(":invalid:"),
+                Err(format!("failed to parse css selector `:invalid:`")),
+            );
+        }
+
+        #[test]
+        fn select() {
+            let doc = scraper::Html::parse_document(
+                r#"
+<!DOCTYPE html>
+<html>
+<body>
+<div class="a">
+    <span>1</span>
+    <span>2</span>
+    <span>3</span>
+</div>
+
+<div class="b">
+    <span>4</span>
+</div>
+
+<span>5</span>
+</body>
+</html>
+        "#,
+            );
+            let elem = doc.root_element();
+            let a_span = HtmlElementRef::select(&elem, &Selector::parse("div.a > span").unwrap());
+            assert_eq!(
+                a_span
+                    .iter()
+                    .map(|e| e.html().to_string())
+                    .collect::<Vec<_>>(),
+                (1..=3)
+                    .map(|s| format!("<span>{s}</span>"))
+                    .collect::<Vec<_>>(),
+            );
+
+            // nested select
+            let b = HtmlElementRef::select(&elem, &Selector::parse(".b").unwrap())[0];
+            let b_span = HtmlElementRef::select(&b, &Selector::parse("span").unwrap());
+            assert_eq!(b_span.len(), 1);
+            assert_eq!(b_span[0].html().to_string(), "<span>4</span>".to_string());
+        }
+
+        #[test]
+        fn text_contents() {
+            let doc =
+                scraper::Html::parse_fragment("<html><div>a<div>b</div><div>c</div></div></html>");
+            let elem = doc.root_element();
+            assert_eq!(elem.text_contents(), "a b c");
+        }
+
+        #[test]
+        fn get_attribute() {
+            let doc = scraper::Html::parse_fragment(r#"<html><div id="foo" class="bar" /></html>"#);
+            let elem = doc
+                .root_element()
+                .select(&Selector::parse("div").unwrap())
+                .next()
+                .unwrap();
+            println!("{}", elem.html().to_string());
+            assert_eq!(elem.get_attribute("id").unwrap(), "foo");
+            assert_eq!(elem.get_attribute("class").unwrap(), "bar");
         }
     }
 }
