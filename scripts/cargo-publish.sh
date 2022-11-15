2b04be07dea6e1833c57b775a4b7a9c3b462664a
naoto.ikeno <naoto.ikeno@dena.jp>
2022-11-15 19:08:39 +0900

    wait 10sec before each cargo-publish

--- scripts/cargo-publish.sh
+++ scripts/cargo-publish.sh
@@ -1,9 +1,14 @@
+set -e
 BASEDIR=$(dirname $0)
 pushd ${BASEDIR}/../core
 cargo publish
+echo "waiting 10sec..."
+sleep 10
 popd
 pushd ${BASEDIR}/../macro
 cargo publish
+echo "waiting 10sec..."
+sleep 10
 popd
 pushd ${BASEDIR}/../
 cargo publish
