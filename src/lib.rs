c08d8092346662011739d990ce024406f36aa721
naoto.ikeno <naoto.ikeno@dena.jp>
2023-04-30 14:55:54 +0900

    update example: use pseudo-classes

--- src/lib.rs
+++ src/lib.rs
@@ -23,66 +23,65 @@
 //!     view_count: usize,
 //!     #[h2s(select = "h2 > a", attr = "href")]
 //!     url: String,
-//!     #[h2s(select = "p.modified-date")]
-//!     modified_date: Option<String>,
 //!     #[h2s(select = "ul > li")]
 //!     tags: Vec<String>,
+//!     #[h2s(select = "ul > li:nth-child(1)")]
+//!     first_tag: Option<String>,
 //! }
 //!
 //! let html = r#"
 //! <html lang="en">
 //! <body>
-//! <div>
-//!     <h1 class="blog-title">My tech blog</h1>
-//!     <div class="articles">
-//!         <div>
-//!             <h2><a href="https://example.com/1">article1</a></h2>
-//!             <div><span>901</span> Views</div>
-//!             <ul><li>Tag1</li><li>Tag2</li></ul>
-//!             <p class="modified-date">2020-05-01</p>
-//!         </div>
-//!         <div>
-//!             <h2><a href="https://example.com/2">article2</a></h2>
-//!             <div><span>849</span> Views</div>
-//!             <ul></ul>
-//!             <p class="modified-date">2020-03-30</p>
-//!         </div>
-//!         <div>
-//!             <h2><a href="https://example.com/3">article3</a></h2>
-//!             <div><span>103</span> Views</div>
-//!             <ul><li>Tag3</li></ul>
-//!         </div>
-//!     </div>
-//! </div>
+//!   <div>
+//!       <h1 class="blog-title">My tech blog</h1>
+//!       <div class="articles">
+//!           <div>
+//!               <h2><a href="https://example.com/1">article1</a></h2>
+//!               <div><span>901</span> Views</div>
+//!               <ul><li>Tag1</li><li>Tag2</li></ul>
+//!           </div>
+//!           <div>
+//!               <h2><a href="https://example.com/2">article2</a></h2>
+//!               <div><span>849</span> Views</div>
+//!               <ul></ul>
+//!           </div>
+//!           <div>
+//!               <h2><a href="https://example.com/3">article3</a></h2>
+//!               <div><span>103</span> Views</div>
+//!               <ul><li>Tag3</li></ul>
+//!           </div>
+//!       </div>
+//!   </div>
 //! </body>
 //! </html>
 //! "#;
 //!
 //! let page: Page = h2s::parse(html).unwrap();
+//!
 //! assert_eq!(page, Page {
-//!     lang: "en".into(),
-//!     blog_title: "My tech blog".into(),
+//!     lang: "en".to_string(),
+//!     blog_title: "My tech blog".to_string(),
 //!     articles: vec![
 //!         Article {
-//!             title: "article1".into(),
-//!             url: "https://example.com/1".into(),
+//!             title: "article1".to_string(),
+//!             url: "https://example.com/1".to_string(),
 //!             view_count: 901,
-//!             modified_date: Some("2020-05-01".into()),
-//!             tags: vec!["Tag1".into(), "Tag2".into()]
+//!             tags: vec!["Tag1".to_string(), "Tag2".to_string()],
+//!             first_tag: Some("Tag1".to_string()),
 //!         },
 //!         Article {
-//!             title: "article2".into(),
-//!             url: "https://example.com/2".into(),
+//!             title: "article2".to_string(),
+//!             url: "https://example.com/2".to_string(),
 //!             view_count: 849,
-//!             modified_date: Some("2020-03-30".into()),
-//!             tags: vec![]
+//!             tags: vec![],
+//!             first_tag: None,
 //!         },
 //!         Article {
-//!             title: "article3".into(),
-//!             url: "https://example.com/3".into(),
+//!             title: "article3".to_string(),
+//!             url: "https://example.com/3".to_string(),
 //!             view_count: 103,
-//!             modified_date: None,
-//!             tags: vec!["Tag3".into()]
+//!             tags: vec!["Tag3".to_string()],
+//!             first_tag: Some("Tag3".to_string()),
 //!         },
 //!     ]
 //! });
