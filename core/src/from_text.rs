e5a14a95cec2b864d7802a3780692945fe0b1591
Naoto Ikeno <ikenox@gmail.com>
2023-07-10 14:52:03 +0900

    flatten modules

--- core/src/from_text.rs
+++ core/src/from_text.rs
@@ -1,6 +1,4 @@
-use crate::Error;
 use std::ffi::OsString;
-
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::num::{
     NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize, NonZeroU128,
@@ -9,16 +7,15 @@ use std::num::{
 use std::path::PathBuf;
 use std::str::FromStr;
 
+use crate::Error;
+
 // TODO cannot implement into third party structs by users
 pub trait FromText: Sized {
     type Error: Error;
     fn from_text(s: &str) -> Result<Self, Self::Error>;
 }
 
-mod impls {
-    use super::*;
-
-    macro_rules! impl_from_text {
+macro_rules! impl_from_text {
         ($($t:ty),*) => {
             $(
                 impl FromText for $t {
@@ -32,46 +29,45 @@ mod impls {
         };
     }
 
-    impl_from_text!(
-        String,
-        // primitives
-        bool,
-        char,
-        usize,
-        u8,
-        u16,
-        u32,
-        u64,
-        u128,
-        isize,
-        i8,
-        i16,
-        i32,
-        i64,
-        i128,
-        f32,
-        f64,
-        // non-zero numerics
-        NonZeroU8,
-        NonZeroU16,
-        NonZeroU32,
-        NonZeroU64,
-        NonZeroU128,
-        NonZeroUsize,
-        NonZeroI8,
-        NonZeroI16,
-        NonZeroI32,
-        NonZeroI64,
-        NonZeroI128,
-        NonZeroIsize,
-        // misc that implements FromStr in the standard library
-        PathBuf,
-        IpAddr,
-        Ipv4Addr,
-        Ipv6Addr,
-        SocketAddr,
-        SocketAddrV4,
-        SocketAddrV6,
-        OsString
-    );
-}
+impl_from_text!(
+    String,
+    // primitives
+    bool,
+    char,
+    usize,
+    u8,
+    u16,
+    u32,
+    u64,
+    u128,
+    isize,
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    f32,
+    f64,
+    // non-zero numerics
+    NonZeroU8,
+    NonZeroU16,
+    NonZeroU32,
+    NonZeroU64,
+    NonZeroU128,
+    NonZeroUsize,
+    NonZeroI8,
+    NonZeroI16,
+    NonZeroI32,
+    NonZeroI64,
+    NonZeroI128,
+    NonZeroIsize,
+    // misc that implements FromStr in the standard library
+    PathBuf,
+    IpAddr,
+    Ipv4Addr,
+    Ipv6Addr,
+    SocketAddr,
+    SocketAddrV4,
+    SocketAddrV6,
+    OsString
+);
